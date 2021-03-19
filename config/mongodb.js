63ae02ddc4badec85de9d89cd5803904505b0d1f
arjun <arjun@method-hub.com>
2021-03-19 12:45:46 +0530

    add connection

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,14 +1,24 @@
 const mangoose= require("mongoose");
 
-mangoose.connect("mongodb://localhost:27017/call-my-mover", {
-  useNewUrlParser:true, 
-   useUnifiedTopology: true,
-   keepAlive: true,
-   useCreateIndex: true,
-   useFindAndModify: false
-  }).then(() => {
-  console.log("Connected to Database");
-  }).catch((err) => {
-    console.log(err);
-      console.log("Not Connected to Database ERROR! ");
-  });
+// mangoose.connect("mongodb://localhost:27017/call-my-mover", {
+//   useNewUrlParser:true, 
+//    useUnifiedTopology: true,
+//    keepAlive: true,
+//    useCreateIndex: true,
+//    useFindAndModify: false
+//   }).then(() => {
+//   console.log("Connected to Database");
+//   }).catch((err) => {
+//     console.log(err);
+//       console.log("Not Connected to Database ERROR! ");
+//   });
+
+const uri = "mongodb+srv://arjunwinklix:ulxO3pZu2YySe0f9@cluster0.v7a1w.mongodb.net/callMyMover?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB Connected…")
+})
+.catch(err => console.log(err))
