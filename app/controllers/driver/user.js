e4dd1fc8642c92d47d5422e761bb18b8addde87e
arjun <arjun@method-hub.com>
2021-04-05 17:01:59 +0530

    resolve error

--- app/controllers/driver/user.js
+++ app/controllers/driver/user.js
@@ -1,5 +1,5 @@
 const User = require('../../models/driver/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authConfig = require('../../authConfig/auth');
 const auth = require('../../authConfig/auth');
