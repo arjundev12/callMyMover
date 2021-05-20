08d943a70be4543c94557b60c4dbe30d237f12c7
arjun <arjun@method-hub.com>
2021-05-20 11:15:12 +0530

    add update miner

--- app/utils/constant.js
+++ app/utils/constant.js
@@ -2,7 +2,7 @@ module.exports = {
 
    basePrice: "50",
    timePerKM: "2",
-   truckLogo: "uploads\truck-2-xxl.png",
+   truckLogo: "uploads/truck-2-xxl.png",
    defaultMsg: 'and easy to do anywhere, even with Node.js',
    defaultSub: 'Sending with SendGrid is Fun',
    defaultMail: 'arjunsinghyed@gmail.com',
